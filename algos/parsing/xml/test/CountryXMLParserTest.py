2e78764364adba336ab08412968e931737fdecf5
Larry Johnson <johnson.larry.l@gmail.com>
2018-07-26 19:50:45 -0500

    Successfully loaded XML and pushed unit test assertions

--- algos/parsing/xml/test/CountryXMLParserTest.py
+++ algos/parsing/xml/test/CountryXMLParserTest.py
@@ -4,13 +4,17 @@ from XMLLoader import XMLLoader
 
 class CountryXMLParserTest(unittest.TestCase):
     def setUp(self):
-        xml_loader = XMLLoader()
-        xml = xml_loader.loadFile("resources/countries.xml")
-        print(xml)
+        self.xml_loader = XMLLoader()
 
-    def testParseCountry(self):
+    def test_assert_XML(self):
+        actual_xml = self.xml_loader.loadFile("resources/countries.xml")
+        expect_xml = "<?xml version=\"1.0\"?><data>    <country name=\"Liechtenstein\">        <rank>1</rank>        <year>2008</year>        <gdppc>141100</gdppc>        <neighbor name=\"Austria\" direction=\"E\"/>        <neighbor name=\"Switzerland\" direction=\"W\"/>    </country>    <country name=\"Singapore\">        <rank>4</rank>        <year>2011</year>        <gdppc>59900</gdppc>        <neighbor name=\"Malaysia\" direction=\"N\"/>    </country>    <country name=\"Panama\">        <rank>68</rank>        <year>2011</year>        <gdppc>13600</gdppc>        <neighbor name=\"Costa Rica\" direction=\"W\"/>        <neighbor name=\"Colombia\" direction=\"E\"/>    </country></data>"
+        self.assertEqual(expect_xml, actual_xml)
+        self.xml = actual_xml
+
+    def test_parse_country(self):
         pass
 
-    def testParseCountries(self):
+    def test_parse_countries(self):
         pass
 
