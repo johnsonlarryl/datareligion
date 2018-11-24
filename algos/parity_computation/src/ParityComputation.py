ce2cbdab612cccc5c8974e58f2b06a9b70591e37
Larry Johnson <johnson.larry.l@gmail.com>
2018-11-24 14:38:48 -0600

    Computing parity of a string or integer

---
+++ algos/parity_computation/src/ParityComputation.py
@@ -0,0 +1,44 @@
+class ParityComputation:
+    def compute(self, bits):
+        parity_bits = 0
+
+        for bit in bits:
+            parity_bits += int(bit)
+
+        parity_error = parity_bits % 2 == 0
+        parity = 0 if parity_error else 1
+
+        return parity
+
+    def compute_brute(self, x:int) -> int:
+        result = 0
+
+        while x:
+            result ^= x & 1
+            x >>= 1
+
+        return result
+
+     def compute_lowest_set_bit_erased(self, x:int) -> int:
+        result = 0
+
+        while x:
+            result ^= 1
+            x&= x -1 # Drops the lowest bit of x
+
+        return result
+
+    def sort_compute(self, bits):
+        parity_bits = 0
+
+        for bit in bits:
+            parity_bits += int(bit)
+
+        parity_error = parity_bits % 2 == 0
+        parity = 0 if parity_error else 1
+
+        return parity
+
+
+
+
---
+++ algos/parity_computation/test/PartityComputationTest.py
@@ -0,0 +1,81 @@
+import unittest2
+from ParityComputation  import ParityComputation
+import time
+
+
+class ParityComputationTest(unittest2.TestCase):
+    def setUp(self):
+        self.parity_computation = ParityComputation()
+
+    def test_compute_with_parity(self):
+        bits = "01010000000111110000000"
+        expect_parity = 1
+        t0 = time.time()
+        actual_parity = self.parity_computation.compute(bits)
+        print("Parity: " + str(actual_parity))
+        t1 = time.time()
+        total_time = t1 - t0
+        print("Total Execution Time - test_compute_with_parity: {}".format(total_time))
+        assert(expect_parity == actual_parity)
+
+    def test_compute_without_parity(self):
+        bits = "01010000000111110000000010100000001110000000"
+        expect_parity = 0
+        t0 = time.time()
+        actual_parity = self.parity_computation.compute(bits)
+        print("Parity: " + str(actual_parity))
+        t1 = time.time()
+        total_time = t1 - t0
+        print("Total Execution Time - test_compute_without_parity: {}".format(total_time))
+        assert(expect_parity == actual_parity)
+
+    def test_compute_brute_without_parity(self):
+        bits = int(325)
+        # 00000001 01000101
+        expect_parity = 0
+        t0 = time.time()
+        actual_parity = self.parity_computation.compute_brute(bits)
+        print("Parity: " + str(actual_parity))
+        t1 = time.time()
+        total_time = t1 - t0
+        print("Total Execution Time - test_compute_brute_without_parity: {}".format(total_time))
+        assert(expect_parity == actual_parity)
+
+    def test_compute_brute_with_parity(self):
+        bits = int(428)
+        # 00000001 10101100
+        expect_parity = 1
+        t0 = time.time()
+        actual_parity = self.parity_computation.compute_brute(bits)
+        print("Parity: " + str(actual_parity))
+        t1 = time.time()
+        total_time = t1 - t0
+        print("Total Execution Time - test_compute_brute_with_parity: {}".format(total_time))
+        assert(expect_parity == actual_parity)
+
+    def test_compute_lowest_set_bit_erased_without_parity(self):
+        bits = int(325)
+        # 00000001 01000101
+        expect_parity = 0
+        t0 = time.time()
+        actual_parity = self.parity_computation.compute_brute(bits)
+        print("Parity: " + str(actual_parity))
+        t1 = time.time()
+        total_time = t1 - t0
+        print("Total Execution Time - test_compute_brute_without_parity: {}".format(total_time))
+        assert(expect_parity == actual_parity)
+
+    def test_compute_lowest_set_bit_erased_with_parity(self):
+        bits = int(428)
+        # 00000001 10101100
+        expect_parity = 1
+        t0 = time.time()
+        actual_parity = self.parity_computation.compute_brute(bits)
+        print("Parity: " + str(actual_parity))
+        t1 = time.time()
+        total_time = t1 - t0
+        print("Total Execution Time - test_compute_brute_with_parity: {}".format(total_time))
+        assert(expect_parity == actual_parity)
+
+
+
